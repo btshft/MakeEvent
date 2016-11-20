124bc185e1559c16d8966ce52d00b728af8b4e6f
Red Fox <redfox9966@gmail.com>
2016-11-21 00:52:35 +0300

    Страница списка мероприятий( без категорий) и список новостей

--- MakeEvent.Web/Scripts/KendoSPA/ViewModels/MainApp/Events.js
+++ MakeEvent.Web/Scripts/KendoSPA/ViewModels/MainApp/Events.js
@@ -4,11 +4,41 @@
         Id: 0,
         Login: 'Test'
     },
+    events: [{
+        Id: 0,
+        Title:'Сдача Курсовой работы',
+        OrgName:'СевГу',
+        Adress:'г. Севастополь, ул. Университетская 33',
+        Date:'20.12.2016',
+        Description: 'Мы будем страдать, страдать и еще раз страдать. Приходите на на наши мероприятия. Не пожалеете.',
+        ImageUrl: '/Content/images/event-icon.png'
+    },
+    {
+        Id: 0,
+        Title: 'Сдача Курсовой работы',
+        OrgName: 'СевГу',
+        Adress: 'г. Севастополь, ул. Университетская 33',
+        Date: '20.12.2016',
+        Description: 'Мы будем страдать, страдать и еще раз страдать. Приходите на на наши мероприятия. Не пожалеете.',
+        ImageUrl: '/Content/images/event-icon.png'
+    },
+    {
+        Id: 0,
+        Title: 'Сдача Курсовой работы',
+        OrgName: 'СевГу',
+        Adress: 'г. Севастополь, ул. Университетская 33',
+        Date: '20.12.2016',
+        Description: 'Мы будем страдать, страдать и еще раз страдать. Приходите на на наши мероприятия. Не пожалеете.',
+        ImageUrl: '/Content/images/event-icon.png'
+    }],
     init: function () {
         this.set('isGuest', !this.isAutorized);
+        $('.DateTime').kendoDatePicker();
+        $('.filter-input').kendoMaskedTextBox();
+        $('.weight-button, .light-button, .default-btn').kendoButton();
     },
     show: function () {
-
+        KendoHelper.insertExtTemplate('#eventfull-item', '#events-block-list', this.events);
     },
     goToPersonal: function () {
         router.navigate('personalPage/' + this.user.Id);
--- MakeEvent.Web/Scripts/KendoSPA/ViewModels/MainApp/News.js
+++ MakeEvent.Web/Scripts/KendoSPA/ViewModels/MainApp/News.js
@@ -4,11 +4,36 @@
         Id: 0,
         Login: 'Test'
     },
+    news: [{
+        Id: 0,
+        Title: 'Открытие организации "MakeEvent"',
+        PreviewUrl: '/Content/images/news-icon.png',
+        ShortDescription: 'Еще чуть-чуть и мы доделаем эту замечательную курсовую работу.',
+        Date: '10.12.2016'
+    },
+ {
+     Id: 0,
+     Title: 'Открытие организации "MakeEvent"',
+     PreviewUrl: '/Content/images/news-icon.png',
+     ShortDescription: 'Еще чуть-чуть и мы доделаем эту замечательную курсовую работу.',
+     Date: '10.12.2016'
+ },
+  {
+      Id: 0,
+      Title: 'Открытие организации "MakeEvent"',
+      PreviewUrl: '/Content/images/news-icon.png',
+      ShortDescription: 'Еще чуть-чуть и мы доделаем эту замечательную курсовую работу.',
+      Date: '10.12.2016'
+  }],
     init: function () {
         this.set('isGuest', !this.isAutorized);
     },
     show: function () {
-
+        KendoHelper.insertExtTemplate('#news-item', '#news-list-block', this.news);
+        $('a.news-link').click(function (event) {
+            var id = $(event.target).data('id');
+            router.navigate('newsitem/' + id);
+        });
     },
     goToPersonal: function () {
         router.navigate('personalPage/' + this.user.Id);
