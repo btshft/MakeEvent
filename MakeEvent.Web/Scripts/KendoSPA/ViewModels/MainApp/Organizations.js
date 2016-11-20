b857829c7c03849375cd355ab6c371d1d16020a2
Red Fox <redfox9966@gmail.com>
2016-11-20 23:37:09 +0300

    Добавлена странца с списком организаций

--- MakeEvent.Web/Scripts/KendoSPA/ViewModels/MainApp/Organizations.js
+++ MakeEvent.Web/Scripts/KendoSPA/ViewModels/MainApp/Organizations.js
@@ -4,13 +4,45 @@
         Id: 0,
         Login: 'Test'
     },
+    filter:{
+        orgName:''
+    },
+    organizations: [{
+        Id: 0,
+        Name: 'ООО "Тест Ивент"',
+        LogoUrl: '/Content/images/org-logo.png',
+        Description: 'Мы занимаемся очень интересными вещами. Приходите на на наши мероприятия. Не пожалеете',
+        Site: 'http://test.ru'
+    },
+    {
+        Id: 0,
+        Name: 'ООО "Тест Ивент"',
+        LogoUrl: '/Content/images/org-logo.png',
+        Description: 'Мы занимаемся очень интересными вещами. Приходите на на наши мероприятия. Не пожалеете',
+        Site: 'http://test.ru'
+    },
+    {
+        Id: 0,
+        Name: 'ООО "Тест Ивент"',
+        LogoUrl: '/Content/images/org-logo.png',
+        Description: 'Мы занимаемся очень интересными вещами. Приходите на на наши мероприятия. Не пожалеете',
+        Site: 'http://test.ru'
+    }],
     init: function () {
         this.set('isGuest', !this.isAutorized);
+        $('[type=text]').kendoMaskedTextBox();
     },
     show: function () {
-
+        KendoHelper.insertExtTemplate('#org-item', '#org-list-block', this.organizations);
+        $('a.org-header').click(function (event) {
+            var id = $(event.target).data('id');
+            router.navigate('organization/' + id);
+        });
     },
     goToPersonal: function () {
         router.navigate('personalPage/' + this.user.Id);
     },
+    searchOrgs: function () {
+
+    }
 })
